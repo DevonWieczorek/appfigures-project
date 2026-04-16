2b459738f6be588eda5260d28ab4097b3eabc1fb
Devon Wieczorek <devon.wieczorek93@gmail.com>
2026-04-16 10:51:46 -0400

    chore: removes withMT from tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from 'tailwindcss'
-import withMT from '@material-tailwind/react/utils/withMT'
 
-const config: Config = withMT({
+const config: Config = {
   content: [
     './index.html',
     './src/**/*.{ts,tsx}',
@@ -14,5 +13,5 @@ const config: Config = withMT({
     },
   },
   plugins: [],
-})
+}
 export default config
