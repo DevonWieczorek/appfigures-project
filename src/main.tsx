21f2dddeb9bcafeff2ddf6a7dac733bf259589dd
Devon Wieczorek <devon.wieczorek93@gmail.com>
2026-04-16 21:47:57 -0400

    feat: adds redirect for non-existent routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import App from './App.tsx'
 import { ErrorBoundary } from "@/components/error-boundary";
 import { AppErrorFallback } from "@/components/app-error-fallback";
@@ -17,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
             </ErrorBoundary>
           )}
         />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
